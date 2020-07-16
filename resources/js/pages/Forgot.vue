5ae8775da53a648b003933128f551be617469a5b
joesama <joharijumali@gmail.com>
2020-07-16 11:53:51 +0800

    add forgot page

---
+++ resources/js/pages/Forgot.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="w-100 my-4">
+        <div class="col-lg-10 col-xl-9 mx-auto">
+            <div class="card card-signin flex-row my-5">
+                <div class="card-img-left d-none d-md-flex">
+                    <!-- Background image for card set in CSS! -->
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Forgot Password</h5>
+                    <form class="form-signin">
+
+                    <div class="form-label-group">
+                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
+                        <label for="inputEmail">Email address</label>
+                    </div>
+
+                    <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Forgot Password</button>
+                    <router-link to="/login"  class="d-block text-center mt-2 small" >Sign In</router-link>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {}
+}
+</script>
