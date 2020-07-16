337924fa1d90688f820d58de795ac81d0eb3f05e
joesama <joharijumali@gmail.com>
2020-07-16 15:03:49 +0800

    update api routing

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('registration', function (Request $request) {
+    dd($request->all());
+});
+
+Route::post('login', function (Request $request) {
+    dd($request->all());
+});
+
+Route::post('forgot', function (Request $request) {
+    dd($request->all());
+});
